074603db3d8ae4d5eb58a115976d9494803ec587
variskadri92 <dzire192@gmail.com>
2024-03-22 23:23:35 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'upi_payment_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Integrate UPI in flutter app",
+      home: UpiPaymentScreen(),
+    );
+  }
+}
